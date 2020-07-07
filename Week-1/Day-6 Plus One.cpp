f3542ab7df72a3c607a4381849bbf1d574f931d8
Jeevan Kumar <63276441+Jeevan-kumar-Raj@users.noreply.github.com>
2020-07-07 07:31:11 +0530

    Add files via upload

---
+++ Week-1/Day-6 Plus One.cpp
@@ -0,0 +1,45 @@
+Day-6 Plus One.cpp
+
+
+/*
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Example 2:
+
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+
+
+*/
+
+
+// Time:  O(n)
+// Space: O(1)
+
+// in-place solution
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; --i) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                ++digits[i];
+                return digits;
+            }
+        }
+        digits[0] = 1;
+        digits.emplace_back(0);
+        return digits;
+    }
+};
